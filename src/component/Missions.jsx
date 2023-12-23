729efe70bec1ba0a4f52bba82961040044b59a9c
Arvin Ken M. Ramirez <107236810+ArvinKenRamirez@users.noreply.github.com>
2023-12-23 20:26:37 +0800

    updated missions component

--- src/component/Missions.jsx
+++ src/component/Missions.jsx
@@ -3,13 +3,14 @@ import FilterMenu from "./FilterMenu";
 import Mission from "./Mission";
 
 export default function Missions({ MISSIONS }){
-    const missionCount =  (MISSIONS.length === 0 ? 'Showing 0 Missions' : `Showing ${MISSIONS.length} Missions`);
     const missionData = MISSIONS;
+    // const finalMissions = new Map(MISSIONS.map(mission => [mission.flight_number, mission]));
+    const [filteredMissions, setFilteredMissions] = new useState(missionData);
+    const missionCount =  (MISSIONS.length === 0 ? 'Showing 0 Missions' : `Showing ${filteredMissions.length} Missions`);
     
     const [dataFromChild, setDataFromChild] = useState({ Keywords:'', LaunchPad: '', MinYear: '', MaxYear: '' });
 
-    // const finalMissions = new Map(MISSIONS.map(mission => [mission.flight_number, mission]));
-    const [filteredMissions, setFilteredMissions] = new useState(missionData);
+    
 
     const handleDataFromChild = (data) => {
         setDataFromChild(data);
@@ -55,3 +56,12 @@ export default function Missions({ MISSIONS }){
     );
     
 }
+
+// EXPERIENCE
+// Arvin has been consistently meeting the expectations set for him. His attention to details are admirable, taking notes of what's being discussed. By this, he is ensuring that he will be able to cover all scenarios of the requirement which truly reflects on his finished product. He is able to deliver and can work with minimal support needed.
+
+
+// Arvin is a great asset for DCPDC ‘s Internal Project that he was assigned in, he is one of the main developer who also handles the code repository and deployment in their projects. He can work independently and has good technical Salesforce expertise, particularly with relation to APEX and LWC.
+
+
+// Arvin is a Salesforce Certified Administrator and has strong experience in handling declarative and programmatic development in the Salesforce platform using various Apex Classes, Triggers, Test Classes, Visualforce pages, Aura, Lightning Web Components, and Flows to implement complex business requirements  from combined onshore/offshore previous projects and trainings.
